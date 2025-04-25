f4fa35abfae200da64582159696f4b553fa60a08
nissenemj <nissenemj@gmail.com>
2025-04-25 21:45:38 +0300

    Fix TypeScript errors in calculator components and imports

--- src/components/calculator/index.ts
+++ src/components/calculator/index.ts
@@ -1,6 +1,5 @@
-
-export * from './DebtConsolidationCalculator';
-export * from './DebtPayoffCalculator';
-export * from './DebtPayoffTimeline';
-export * from './ExtraPaymentCalculator';
-export * from './DebtVisualization';
+// Export all named exports from calculator components
+export { DebtConsolidationCalculator } from "./DebtConsolidationCalculator";
+export { DebtPayoffCalculator } from "./DebtPayoffCalculator";
+export { DebtPayoffTimeline } from "./DebtPayoffTimeline";
+export { ExtraPaymentCalculator } from "./ExtraPaymentCalculator";
